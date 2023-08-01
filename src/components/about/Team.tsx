0c7a313d4954b5df32268d45af76a37d14d5e7e4
Obi Pascal <pascalobi83@gmail.com>
2023-08-01 15:16:21 +0100

    updated

--- src/components/about/Team.tsx
+++ src/components/about/Team.tsx
@@ -35,26 +35,41 @@ export default function Team({ content }: TeamProps) {
 					</div>
 					<div className="grid xs:grid-cols-1 md:grid-cols-3 gap-8">
 						<div className="relative group/card transition ease-in duration-300 min-h-10">
-							<Image src={LilianGareldImg} alt="Lilian Garald" className="rounded-md w-80  transition ease-in duration-300 hover:scale-[1.1]" />
-							<div className="rounded-md bg-gradient-to-r from-indigo-500 to-blue-900 my-4 p-3 w-fit">
+							<Image src={LilianGareldImg} alt="Lilian Garald" className="rounded-md w-80 m-auto block  transition ease-in duration-300 hover:scale-[1.1]" />
+							<div className="rounded-md bg-gradient-to-r from-indigo-500 to-blue-900 my-4 p-3 w-full">
 								<h1 className="font-bold xs:text-[16px] md:text-[22px] text-white uppercase">LILIAN GERALD</h1>
 								<h2 className="font-medium xs:text-[20px] md:text-[20px] text-gray-500 capitalize italic text-secondary">-Social Media Manager</h2>
+								<hr className="my-4" />
+								<p className="font-medium text-md text-gray-100 italic text-justify">
+									Gerald is the creative force behind JCK Consulting&apos;s social media presence. With a keen eye for storytelling and a passion for community engagement, she brings the
+									company&apos;s mission to life through captivating content.
+								</p>
 							</div>
 						</div>
 
 						<div className="relative group/card transition ease-in duration-300 min-h-10">
-							<Image src={AyorindeOmoJuwa} alt="Ayorinde Omojuwa" className="rounded-md w-80  transition ease-in duration-300 hover:scale-[1.1]" />
-							<div className="rounded-md bg-gradient-to-r from-indigo-500 to-blue-900 my-4 p-3 w-fit">
+							<Image src={AyorindeOmoJuwa} alt="Ayorinde Omojuwa" className="rounded-md w-80 m-auto block  transition ease-in duration-300 hover:scale-[1.1]" />
+							<div className="rounded-md bg-gradient-to-r from-indigo-500 to-blue-900 my-4 p-3 w-full">
 								<h1 className="font-bold xs:text-[16px] md:text-[22px] text-white uppercase">Ayorinde Omojuwa</h1>
 								<h2 className="font-medium xs:text-[20px] md:text-[20px] text-gray-500 capitalize italic text-secondary">-Coding Tutor</h2>
+								<hr className="my-4" />
+								<p className="font-medium text-md text-gray-100 italic text-justify">
+									Versatile Coding Tutor at JCK Consulting. With expertise in tech and a passion for empowering learners, I play a pivotal role in guiding aspiring tech enthusiasts and engaging our
+									online community.
+								</p>
 							</div>
 						</div>
 
 						<div className="relative group/card transition ease-in duration-300 min-h-10">
-							<Image src={ObiPascalImg} alt="Obi Pascal Banjuare" className="rounded-md w-80 transition ease-in duration-300 hover:scale-[1.1]" />
-							<div className="rounded-md bg-gradient-to-r from-indigo-500 to-blue-900 my-4 p-3 w-fit">
+							<Image src={ObiPascalImg} alt="Obi Pascal Banjuare" className="rounded-md w-80 m-auto block transition ease-in duration-300 hover:scale-[1.1]" />
+							<div className="rounded-md bg-gradient-to-r from-indigo-500 to-blue-900 my-4 p-3 w-full">
 								<h1 className="font-bold xs:text-[16px] md:text-[22px] text-white uppercase">OBI PASCAL</h1>
 								<h2 className="font-medium xs:text-[20px] md:text-[20px] text-gray-500 capitalize italic text-secondary">-Software Developer</h2>
+								<hr className="my-4" />
+								<p className="font-medium text-md text-gray-100 italic text-justify">
+									Accomplished Full-stack Software Developer with 6 years of expertise, committed to delivering innovative solutions for clients. Passionate about creating efficient and user-friendly
+									applications, they boast a stellar record of high-quality project delivery within time and budget constraints.
+								</p>
 							</div>
 						</div>
 					</div>
